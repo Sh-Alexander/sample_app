7446ddbb05a2eb1ee59754ec9b5b89da1fdfe66d
Sh-Alexander <Fafa.89@mail.ru>
2021-12-29 14:34:15 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root to: 'static_page#home'
-  get 'home' => 'static_page/home'
+  get 'home' => 'static_page#home'
   get 'help' => 'static_page#help'
   get 'about' => 'static_page#about'
   get 'contact' => 'static_page#contact'
+  get 'signup' => 'users#new' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
