3cc909cd39bf0847476059d6d064def1dc648c24
Alexey Nikitin <tank@bohr.su>
2022-02-27 15:14:21 +0300

    fix bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import  { Dropdown } from "bootstrap" 
-
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
