28feb52e31ad10e0cb452bae19e37f7fdbe8d855
Alexey Nikitin <tank@bohr.su>
2022-02-27 15:43:11 +0300

    Uskip test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
-    skip "PIZDA POLNAIA"
+  def setup
+    @user = User.create!(name: "Example User", email: "user@example.com",
+            password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "login with valid information" do
     get login_path
     assert_template 'sessions/new'
-    post (login_path( session: { email: "", password: "" }))
+    post login_path, params: {session: {email: "user@example.com", password: "foobar"}}
+    assert_redirected_to user_path(@user)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {session: {email: "", password: ""}}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
